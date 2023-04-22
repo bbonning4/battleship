15c61a38d9063d188bc93917315d4fdf1383c46f
bbonning4 <bbonning42@gmail.com>
2023-04-21 20:06:26 -0500

    code skeleton

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
+// define constants
+// define variables used to track game state
+
 // initialize 2 boards, including their "ships"
-// init() will create boards using a Board class
-// ships will be initialized in the constructor
+    // init() will create boards using a Board class
+    // ships will be initialized in the constructor
 
-// loop through ships to have player and ai place ships onto board
-// player will click a square to "begin" placing, and will click an "end" depending on length of ship
-// repeat until all ships are successfully placed(ships arrays won't contain null)
+// loop through ships array to have player and ai place ships onto board
+    // player will click a square to "begin" placing, and will click an "end" depending on length of ship
+    // prevent overlapping ships or going off board
+    // allow player/ai to leave placement in case initial spot won't fit the ship
+    // update board array with ship layout
 
-// alternate hitting the other board by clicking a tile on opponent's board
-// change tile whether a "hit"(backgroundColor='red') or "miss"(x) occurs
-// for the ai, if a hit is made it will attempt to hit around that tile until a "sink" occurs
+// alternate hitting the other board by clicking a tile on opponent's board --handleShot()
+    // ai will handleShot by initially selecting random squares until a hit
+    // develop ai logic that will attempt to sink a ship after a hit
+    // change tile whether a "hit"(backgroundColor='red') or "miss"(x) occurs
 
 // update "ships" frame if a hit or sink occurs
-// once all ships are sunk, that player loses
+
+// once all ships for a player has sunk, the winner will be determined at the end of the turn
+    // if both players have all ships sunk at the end of turn, draw
 
 // can restart a new game by clicking a button
+
+
+/*----- constants -----*/
+
+/*----- state variables -----*/
+let playerBoard;
+let aiBoard;
+let winner = null;
 
 class Board {
     constructor() {
@@ -32,27 +48,49 @@ class Board {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
+        ];
     }
 }
 
-// variables
-let playerBoard;
-let aiBoard;
 
+/*----- cached elements  -----*/
+const $msgEl = $('#msg');
+const $resetBtn = $('button');
+const $playerSquares = $('#player-board > div').toArray();
+const $aiSquares = $('#ai-board > div').toArray();
+
+/*----- event listeners -----*/
+// $('#player-board').on('click', 'div', placeShips)
+$('#ai-board').on('click', 'div', handleShot)
+
+
+/*----- functions -----*/
 // initialize board on refresh
 init()
 
-// functions
 function init() {
-    renderBoard()
     createBoards()
     placeShips()
+    render()
 }
 
-function renderBoard() {
+function render() {
+    renderBoards()
+    renderMessage()
+    renderControls()
+}
+
+function renderBoards() {
     // clear all tiles and ships
     // or update
+}
+
+function renderMessage() {
+
+}
+
+function renderControls() {
+    
 }
 
 function createBoards() {
@@ -62,4 +100,30 @@ function createBoards() {
 
 function placeShips() {
 
+}
+
+function handleShot(evt) {
+    // can put these up top?
+    let x;
+    let y;
+    const sqr = evt.target;
+    const sqrIdx = $aiSquares.indexOf(sqr);
+    if(sqrIdx === -1) return;
+    
+    x = sqrIdx % 10;
+    y = Math.floor(sqrIdx/10);
+    // 0,0 in top left corner 9,9 in bottom right
+    console.log(x, y)
+
+    // ai shoots next
+    aiTurn()
+    getWinner()
+}
+
+function aiTurn() {
+    
+}
+
+function getWinner() {
+    console.log(winner)
 }
