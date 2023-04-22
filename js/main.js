ee2b763302e507148ac0f7341c4e0aedffa53002
bbonning4 <bbonning42@gmail.com>
2023-04-22 05:14:06 -0500

    basic render 2 ships

--- js/main.js
+++ js/main.js
@@ -25,6 +25,17 @@
 
 
 /*----- constants -----*/
+const TILES = {
+    '0': 'lightskyblue',
+    '1': 'darkgrey'
+}
+
+const RANDOM_DIRECTION = {
+    '0': [-1, 0],
+    '1': [1, 0],
+    '2': [0, -1],
+    '3': [0, 1]
+}
 
 /*----- state variables -----*/
 let playerBoard;
@@ -83,6 +94,10 @@ function render() {
 function renderBoards() {
     // clear all tiles and ships
     // or update
+    $playerSquares.forEach(sqr => {
+        let coords = getXY(sqr, $playerSquares)
+        sqr.style.backgroundColor = TILES[playerBoard.board[coords.x][coords.y]]
+    })
 }
 
 function renderMessage() {
@@ -98,22 +113,84 @@ function createBoards() {
     aiBoard = new Board();
 }
 
+
 function placeShips() {
-
-}
-
-function handleShot(evt) {
-    // can put these up top?
     let x;
     let y;
-    const sqr = evt.target;
-    const sqrIdx = $aiSquares.indexOf(sqr);
-    if(sqrIdx === -1) return;
-    
-    x = sqrIdx % 10;
-    y = Math.floor(sqrIdx/10);
+    // player ships placed
+    playerBoard.ships.forEach((ship, shipIdx) => {
+        let segments = 0;
+        ship.forEach(function place(segment, idx) { 
+            if(segments > 0) {
+                // randomise one of the 4 directions the next segment can go
+                let dxdy = Math.floor(Math.random() * 4)
+                let newCoords = {
+                    'x': x + RANDOM_DIRECTION[dxdy][0],
+                    'y': y + RANDOM_DIRECTION[dxdy][1]
+                }
+                if (newCoords.x >=0 && newCoords.x < 10 &&
+                    newCoords.y >=0 && newCoords.y < 10) {
+                        if(playerBoard.board[newCoords.x][newCoords.y] === 0) {
+                            playerBoard.board[newCoords.x][newCoords.y] = 1;
+                            playerBoard.ships[shipIdx][idx] = 1;
+                        }
+                    }
+                else {
+                    place(segment, idx)
+                }
+                x = newCoords.x;
+                y = newCoords.y;
+                segments += 1;
+            }
+            if(playerBoard.ships[shipIdx][idx] === null) {
+                x = Math.floor(Math.random() * 10)
+                y = Math.floor(Math.random() * 10)
+                if(playerBoard.board[x][y] === 0) {
+                    playerBoard.board[x][y] = 1;
+                    playerBoard.ships[shipIdx][idx] = 1;
+                }
+                else {
+                    place(segment, idx)
+                }
+                segments += 1;
+            }
+        })
+    })
+    // ai ships placed
+    aiBoard.ships.forEach((ship, shipIdx) => {
+        ship.forEach(function place(segment, idx) {
+            if(segment === null) {
+                let x = Math.floor(Math.random() * 10)
+                let y = Math.floor(Math.random() * 10)
+                if(aiBoard.board[x][y] === 0) {
+                    aiBoard.board[x][y] = 1;
+                    aiBoard.ships[shipIdx][idx] = 1;
+                }
+                else {
+                    place(segment, idx)
+                }
+            }
+        })
+    })
+}
+// // attempt at making player choose their positions
+// while(segment === null) {
+//     const clickHandle = (evt) => {
+//         let coords = getXY(evt, $playerSquares)
+//         if(playerBoard.board[coords.x][coords.y] === 0) {
+//             playerBoard.board[coords.x][coords.y] = 1;
+//             segment = 1;
+//             $('#player-board').off('click', 'div', clickHandle);
+//         }
+//     }
+// }
+// $('#player-board').on('click', 'div', clickHandle);
+
+
+
+function handleShot(evt) {
+    let coords = getXY(evt, $aiSquares)
     // 0,0 in top left corner 9,9 in bottom right
-    console.log(x, y)
 
     // ai shoots next
     aiTurn()
@@ -125,5 +202,15 @@ function aiTurn() {
 }
 
 function getWinner() {
-    console.log(winner)
+    // console.log(winner)
+}
+
+function getXY(evt, $squares) {
+    let x;
+    let y;
+    // const sqr = evt.target;
+    const sqrIdx = $squares.indexOf(evt);
+    x = sqrIdx % 10;
+    y = Math.floor(sqrIdx/10);
+    return {'x':x, 'y':y};
 }
